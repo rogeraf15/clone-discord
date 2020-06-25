aa9a661b28e4e36536fcc0376247123e5be9904b
rogeraf15 <roger.afernandes@hotmail.com>
2020-06-25 16:57:44 -0300

    Criando Projeto
    Criei o Layout do Projeto
    Criei a Lista de Servidores com o nome e  os botões
    Cirei a Informação do Canal

---
+++ src/Components/ServerName/index.tsx
@@ -0,0 +1,17 @@
+import  React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles'
+
+
+
+const ServerName: React.FC = () => {
+    return (
+        <Container>
+            <Title>Nome do Servidor</Title>
+
+            <ExpandIcon />
+        </Container>
+    );
+};
+
+export default ServerName;
