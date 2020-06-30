1b917ac81bf922f71c1c1d1bcee88a2d517d4eb6
rogeraf15 <roger.afernandes@hotmail.com>
2020-06-29 23:16:58 -0300

    Script para Descer o Scroll quando recarregar a pagina
    No padrão quando entra na pagina o scroll fica bem no topo, esse script e para baixar o scroll, para que apareça as ultimas mensagens

--- src/Components/ChannelData/index.tsx
+++ src/Components/ChannelData/index.tsx
@@ -1,4 +1,4 @@
-import  React from 'react';
+import  React, {useRef, useEffect } from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
 import ChannelMessage, {Mention} from '../ChannelMessage';
@@ -6,10 +6,20 @@ import ChannelMessage, {Mention} from '../ChannelMessage';
 
 
 const ChannelData: React.FC = () => {
+    
+    // Para quando recarregar pagina o scroll das mensagens comece para baixo, e nao para cima, como no padrão.
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+    useEffect(() => {
+        const div = messagesRef.current;
+
+        if(div) {
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef]);
 
     return (
         <Container>
-            <Messages>
+            <Messages ref={messagesRef}>
                 <ChannelMessage
                     author = "Roger Fernandes"
                     date = "29/06/2020"
